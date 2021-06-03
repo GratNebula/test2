4743660f8168202869296a542230175582f41e16
Quentin DOUMERC <q.doumerc@net.estia.fr>
2021-06-03 14:49:10 +0200

    v2

--- projet-ausy-git2/src/Test2.java
+++ projet-ausy-git2/src/Test2.java
@@ -3,8 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("projet ausy git 2 version 1");
-		System.out.println("projet ausy git 2 version 2");
-		System.out.println("projet ausy git 2 version 3");
+
 
 	}
 
